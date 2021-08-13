d5251458d9642a6f68afc782001ed0a63b899ce5
unknown <vcnts0@gmail.com>
2021-08-13 17:04:24 -0600

    routin

--- src/components/BaseNav/index.js
+++ src/components/BaseNav/index.js
@@ -1,9 +1,5 @@
 import React from 'react';
-import { Route, Switch } from "react-router-dom";
-
-import Account from '../Account';
-import Calendar from '../Calender';
-
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 import { makeStyles } from '@material-ui/core/styles';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
@@ -11,6 +7,9 @@ import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import AccountBoxIcon from '@material-ui/icons/AccountBox';
 import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+
+import Account from '../Account';
+import Calender from '../Calender';
 
 const useStyles = makeStyles({
   root: {
@@ -23,7 +22,7 @@ export default function SimpleBottomNavigation() {
   const [value, setValue] = React.useState(0);
 
   return (
-    
+    <Router>
     <BottomNavigation
       value={value}
       onChange={(event, newValue) => {
@@ -32,10 +31,18 @@ export default function SimpleBottomNavigation() {
       showLabels
       className={classes.root}
     >
+      <Link to="/account" component={Account}>
       <BottomNavigationAction label="Account" icon={<AccountBoxIcon />} />
+      </Link>
+      <Link to="/calender" component={Calender}>
       <BottomNavigationAction label="Calender" icon={<CalendarTodayIcon />} />
+      </Link>
       <BottomNavigationAction label="Log Out" icon={<ExitToAppIcon />} />
+      <Switch>
+      <Route path="/account" exact component={Account} />
+      <Route path="/calender"  component={Calender} />
+      </Switch>
     </BottomNavigation>
-    
+    </Router>
   );
 }
